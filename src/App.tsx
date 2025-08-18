6db5e8502c502ef49491f975efa00b11450094a3
RenShadecrest <michaelaaronphoto@gmail.com>
2025-08-17 21:14:42 -0700

    Initiated react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './layout/Layout';
 import './App.css'
 
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [],
+  },
+]);
+
+
+
 function App() {
+  
   return (
     <>
+      <RouterProvider router={router}/>
     </>
   );
 };
